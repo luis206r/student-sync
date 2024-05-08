7c5147e98eecad76441792d0686647c55b7db4ac
luis206r <luisr_02@outlook.es>
2024-05-07 19:07:12 -0500

    primer avance

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{jsx,tsx}", "./index.html"],
+  darkMode: "selector",
+  theme: {
+    extend: {
+      colors: {
+        "cach-l1": "#F3F3F3",
+        "cach-l2": "#cbb7f7",
+        "cach-l3": "#7f6afc",
+        "cach-l4": "#2b2c41",
+        "cach-l5": "#797979",
+        "textcol-1": "#7E7A7A",
+      },
+    },
+  },
+  //plugins: ["prettier-plugin-tailwindcss"],
+};
