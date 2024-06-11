483411045afdf6f937e84ef78e2cb569b099a1ab
luis206r <luisr_02@outlook.es>
2024-06-11 17:06:33 -0500

    asdasdasd

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePluginRadar({
+    // Google Analytics tag injection
+    analytics: {
+      id: 'G-56X83C2ZX7',
+    },
+  })],
 })
